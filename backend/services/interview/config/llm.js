ef14aa3276029d63305c811cf5462f034f426509
summar22 <summarporwal22@gmail.com>
2026-09-23 16:40:31 +0530

    feat(interview): implement multi-agent AI mock interview system using LangGraph

---
+++ backend/services/interview/config/llm.js
@@ -0,0 +1,12 @@
+
+import { ChatGroq } from "@langchain/groq"
+import dotenv from "dotenv"
+dotenv.config()
+
+const llm = new ChatGroq({
+    model: "llama-3.3-70b-versatile",
+    temperature: 0.2,
+    maxRetries: 2,
+})
+
+export default llm
